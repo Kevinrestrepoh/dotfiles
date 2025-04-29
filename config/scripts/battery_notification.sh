01ccc3088e9f5d4df8301835c73a2755d71b55fd
Kevinrestrepoh <kevinrestrepoh@gmail.com>
2025-04-29 14:47:24 -0500

    fixed battery notification sh

--- config/scripts/battery_notification.sh
+++ config/scripts/battery_notification.sh
@@ -1,4 +1,5 @@
 battery_file="/sys/class/power_supply/BAT0/capacity"
+status_file="/sys/class/power_supply/BAT0/status"
 battery_alert="$HOME/.config/scripts/icons/battery-alert.png"
 battery_icon="$HOME/.config/scripts/icons/battery.png"
 
@@ -6,18 +7,21 @@ last_notification=0
 
 upower --monitor | while read -r line; do
     battery_percentage=$(cat "$battery_file")
-    
-    if [ "$battery_percentage" -eq 5 ] && [ "$last_notification" != 1 ]; then
-        notify-send "Low Battery" "$battery_percentage% of battery remaining" -i "$battery_alert" -t 5000
-        last_notification=1
-    elif [ "$battery_percentage" -eq 20 ] && [ "$last_notification" != 2 ]; then
-        notify-send "Low Battery" "$battery_percentage% of battery remaining" -i "$battery_alert" -t 5000
-        last_notification=2
-    elif [ "$battery_percentage" -eq 100 ] && [ "$last_notification" != 3 ]; then
-        notify-send "Battery Charged" "Battery is fully charged" -i "$battery_icon" -t 5000
-        last_notification=3
-    
+    battery_status=$(cat "$status_file")
+
+    if [ "$battery_status" = "Discharging" ]; then
+      if [ "$battery_percentage" -eq 5 ] && [ "$last_notification" != 1 ]; then
+          notify-send "Low Battery" "$battery_percentage% of battery remaining" -i "$battery_alert" -t 5000
+          last_notification=1
+      elif [ "$battery_percentage" -eq 20 ] && [ "$last_notification" != 2 ]; then
+          notify-send "Low Battery" "$battery_percentage% of battery remaining" -i "$battery_alert" -t 5000
+          last_notification=2
+    elif [ "$battery_status" = "Charging" ] && [ "$battery_percentage" -eq 100 ] && [ "$last_notification" != 3 ]; then
+          notify-send "Battery Charged" "Battery is fully charged" -i "$battery_icon" -t 5000
+          last_notification=3
+    fi
+
     elif [ "$battery_percentage" -gt 20 ] && [ "$battery_percentage" -lt 100 ]; then
         last_notification=0
     fi
-done
+doneone
