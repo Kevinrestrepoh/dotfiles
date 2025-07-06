f59420ffd9b60f1f0b33ced33a13bc8ae92254ee
Kevinrestrepoh <kevinrestrepoh@gmail.com>
2025-07-06 17:46:52 -0500

    fix battery script

--- config/scripts/battery_notification.sh
+++ config/scripts/battery_notification.sh
@@ -16,7 +16,7 @@ upower --monitor | while read -r line; do
       elif [ "$battery_percentage" -eq 20 ] && [ "$last_notification" != 2 ]; then
           notify-send "Low Battery" "$battery_percentage% of battery remaining" -i "$battery_alert" -t 5000
           last_notification=2
-    elif [ "$battery_status" = "Full" ] && [ "$last_notification" != 3 ]; then
+    elif [ "$battery_percentage" -eq 100 ] && [ "$last_notification" != 3 ]; then
         notify-send "Battery Charged" "Battery is fully charged" -i "$battery_icon" -t 5000
         last_notification=3
     fi
