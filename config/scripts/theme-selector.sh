2c9e3da2b3e45b48f266874866dce1791cc78682
Kevinrestrepoh <kevinrestrepoh@gmail.com>
2025-07-13 20:34:49 -0500

    add wallpaper selector

--- config/scripts/theme-selector.sh
+++ config/scripts/theme-selector.sh
@@ -1,9 +1,28 @@
-THEME=$(ls ~/.config/themes | rofi -dmenu -no-custom -theme ~/.config/rofi/theme-selector.rasi)
+OPTION=$(echo -e "Themes\nWallpapers" | rofi -dmenu -theme ~/.config/rofi/theme-selector.rasi -p "Seleccionar:")
 
-if [ -n "$THEME" ]; then
-  ~/.config/scripts/change-theme.sh "$THEME"
+if [ -z "$OPTION" ]; then
+    exit 0
 fi
 
-if [ -n "$THEME" ]; then
-  ~/.config/scripts/change-theme.sh "$THEME"
-fi
+case $OPTION in
+    "Themes")
+        THEME=$(ls ~/.config/themes | rofi -dmenu -theme ~/.config/rofi/theme-selector.rasi -p "Temas:")
+        if [ -n "$THEME" ]; then
+            ~/.config/scripts/change-theme.sh "$THEME"
+        fi
+        ;;
+    "Wallpapers")
+        WALLPAPER=$(
+            find ~/Pictures/wallpapers -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | while read -r img; do
+                echo -en "$img\x00icon\x1f$img\n"
+            done | rofi -dmenu -show-icons -theme ~/.config/rofi/wallpaper-selector.rasi -selected-row 0
+        )
+
+        if [ -n "$WALLPAPER" ]; then
+            killall hyprpaper 2>/dev/null
+            echo "preload = $WALLPAPER" > ~/.config/hypr/hyprpaper.conf
+            echo "wallpaper = ,$WALLPAPER" >> ~/.config/hypr/hyprpaper.conf
+            hyprpaper &
+        fi
+        ;;
+esac
