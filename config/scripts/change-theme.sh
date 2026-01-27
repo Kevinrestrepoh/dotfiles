af5c55b0c6df119e793e19987cf1b7100608535c
Kevinrestrepoh <kevinrestrepoh@gmail.com>
2026-01-27 15:38:24 -0500

    organize structure of themes

--- config/scripts/change-theme.sh
+++ config/scripts/change-theme.sh
@@ -6,12 +6,7 @@ cp "$THEME_DIR/waybar.css" "$HOME/.config/waybar/style.css"
 cp "$THEME_DIR/waybar-config" "$HOME/.config/waybar/config"
 
 # Rofi
-cp "$THEME_DIR/config.rasi" "$HOME/.config/rofi/config.rasi"
-cp "$THEME_DIR/theme-selector.rasi" "$HOME/.config/rofi/theme-selector.rasi"
-cp "$THEME_DIR/powermenu.rasi" "$HOME/.config/rofi/powermenu.rasi"
-cp "$THEME_DIR/wallpaper-selector.rasi" "$HOME/.config/rofi/wallpaper-selector.rasi"
-cp "$THEME_DIR/options.rasi" "$HOME/.config/rofi/options.rasi"
-cp -r "$THEME_DIR/options" "$HOME/.config/rofi"
+cp -r "$THEME_DIR/rofi" "$HOME/.config"
 
 # Kitty
 cp "$THEME_DIR/kitty.conf" "$HOME/.config/kitty/kitty-style.conf"
--- config/scripts/options.sh
+++ config/scripts/options.sh
@@ -1,4 +1,4 @@
-options="󰍹  Monitor\n󰁹  Battery"
+options="󰏘  Theme\n󰋩  Wallpaper\n󰍹  Monitor\n󰁹  Battery"
 
 choice=$(echo -e "$options" | rofi -dmenu -config ~/.config/rofi/options.rasi)
 
@@ -8,5 +8,48 @@ case "$choice" in
         ;;
     *Battery*)
         ~/.config/scripts/options/battery.sh
+        ;;
+    *Theme*)
+        THEME=$(ls ~/.config/themes | rofi -dmenu -config ~/.config/rofi/options/theme-selector.rasi)
+        if [ -n "$THEME" ]; then
+            ~/.config/scripts/change-theme.sh "$THEME"
+        fi
+        ;;
+    *Wallpaper*)
+        WALLPAPER=$(
+            while IFS= read -r img; do
+                echo -en "$(basename "$img")\x00icon\x1f$img\n"
+            done < <(find ~/Pictures/wallpapers -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \)) | 
+            rofi -dmenu -show-icons -config ~/.config/rofi/options/wallpaper-selector.rasi -selected-row 0
+        )
+
+        if [ -n "$WALLPAPER" ]; then
+            FULL_PATH=$(find ~/Pictures/wallpapers -name "$WALLPAPER" -print -quit)
+            if [ -n "$FULL_PATH" ]; then
+                killall hyprpaper 2>/dev/null
+                cat > ~/.config/hypr/hyprpaper.conf <<EOF
+splash = false
+
+wallpaper {
+    monitor = eDP-1
+    path = $FULL_PATH
+  }
+EOF
+                hyprpaper &
+
+                HYPRLOCK_CONF=~/.config/hypr/hyprlock.conf
+                TMP_FILE=$(mktemp)
+                awk -v wallpaper="$FULL_PATH" '
+                    /^background {/ {in_background=1}
+                    in_background && /path = / {
+                        print "    path = " wallpaper
+                        next
+                    }
+                    in_background && /}/ {in_background=0}
+                    {print}
+                ' "$HYPRLOCK_CONF" > "$TMP_FILE"
+                mv "$TMP_FILE" "$HYPRLOCK_CONF"
+            fi
+        fi
         ;;
 esac
